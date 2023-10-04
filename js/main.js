3b0f29bbc77d3be364e380f1eed636446bea12af
Agostina Lamboglia <agoslamboglia@gmail.com>
2023-10-04 19:13:50 -0300

    avances

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//array de productos
+const productos = [
+    {id:1, titulo:"pantalon", precio:12000, imagen: "./img/pantalon1.jpg"},
+    {id:2, titulo:"remera", precio:6000, imagen: "./img/remera1.jpg"},
+    {id:3, titulo:"buzo", precio:15000, imagen: "./img/buzo1.jpg"},
+    {id:4, titulo:"zapatillas", precio:20000, imagen: "./img/zapatillas1.jpg"},
+];
+
